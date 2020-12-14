5001bfea084bd264c3da9e59af357f4bbf3007a2
Can Okay <canokay2014@gmail.com>
2020-12-14 03:51:35 +0300

    0.4.0 - 404  vue router page created.

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p>
+      404
+    </p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound',
+}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import NewPost from "./components/NewPost";
 import Posts from "./components/Posts";
 import PostDetail from "./components/PostDetail";
+import PageNotFound from "./components/PageNotFound";
 
 export const routes = [
     { path : "/", component : Posts, name : "homepage"},
     { path : "/new", component : NewPost, name : "newPost"},
     { path : "/:id", component : PostDetail, name : "post_detail"},
+    { path: "*", component: PageNotFound }
   ]
